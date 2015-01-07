68f2b3ae2c28520509837724e18f7d36a3c0c0c2
ckjck <1025447575@qq.com>
2015-01-07 13:44:53 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+void message (void)
+{
+printf("This is a message!\n");
+}
+
+int main (int argc,char *argv[])
+{
+printf("Hello World!\n");
+message();
+return 0;
+} 
+
